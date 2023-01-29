6d73b2927772b1d0e982cc2b79e0c82542bf3b26
Heidi Ahlberg <ahlberg.heidi@gmail.com>
2023-01-29 19:29:19 +0200

    initial workin version

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,99 @@
 'use strict';
 import * as vscode from 'vscode';
+import { formatSql } from 'poor-mans-t-sql-formatter';
 
-export function activate(context: vscode.ExtensionContext) {
+export const activate = (context: vscode.ExtensionContext) => {
 
     console.log('Congratulations, your extension "sql-linter-ads" is now active!');
-    context.subscriptions.push(vscode.commands.registerCommand('sql-linter-ads.helloWorld', () => {
-        vscode.window.showInformationMessage('Hello World!');
-    }));
-
-    vscode.languages.registerDocumentFormattingEditProvider('sql-lang', {
-        provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
-            const firstLine = document.lineAt(0);
-            if (firstLine.text !== '42') {
-                return [vscode.TextEdit.insert(firstLine.range.start, '42\n')];
-            }
-            throw new Error('Formatting provider load failed');
-        }
+    const formatDisposable = vscode.commands.registerTextEditorCommand('poorSql2.format', function (activeEditor, activeEditorEdit) {
+        formatWithOptions(activeEditor, activeEditorEdit, getFormatterOptions());
     });
-}
+    context.subscriptions.push(formatDisposable);
+
+    vscode.languages.registerDocumentFormattingEditProvider('sql', {
+        provideDocumentFormattingEdits(document: vscode.TextDocument)  {
+            const active = vscode.window.activeTextEditor;
+            if(active){
+                const { range } = getTextInfo(active);
+                const options = getFormatterOptions();
+                vscode.window.showInformationMessage(options.spacesPerTab.toString());
+                const transformed = formatSql(document.getText(), options || getFormatterOptions());
+                if (transformed.errorFound) {
+                    vscode.window.showInformationMessage('There was an error while formatting your SQL.');
+                } else {
+                    return [vscode.TextEdit.replace(range,transformed.text)];
+                }
+            }
+            
+        }
+    });    
+};
+
+const formatWithOptions = (activeEditor: vscode.TextEditor, activeEditorEdit: vscode.TextEditorEdit, options:vscode.FormattingOptions) => {
+    const { text, range } = getTextInfo(activeEditor);
+    const transformed = formatSql(text, options || getFormatterOptions());
+
+    if (transformed.errorFound) {
+        vscode.window.showInformationMessage('There was an error while formatting your SQL.');
+    } else {
+        return [activeEditorEdit.replace(range,transformed.text)];
+    }
+};
+
+
+const getTextInfo = (activeEditor:vscode.TextEditor) => {
+    const { selection, document } = activeEditor;
+    let { start, end } = selection;
+
+    if (start.line > end.line || (start.line === end.line && start.character >= end.character)) {
+        ({ start, end } = getRangeForDocument(document));
+    }
+    
+    const range = new vscode.Range(start.line, start.character, end.line, end.character);
+    
+    const text = document.getText(range);
+
+    return {
+        text,
+        range
+    };
+};
+
+const getFormatterOptions = (isObfuscate = false):vscode.FormattingOptions => {
+    const settings = vscode.workspace.getConfiguration('poorSql2');
+
+    const options = Object.assign({}, settings);
+    const addOnn = {indent: options.tabIndent ? '\t' : ' '.repeat(options.numIndentSpaces || 4),
+    spacesPerTab: options.numIndentSpaces};
+    
+    const newOptions = {...options,...addOnn};
+    if (isObfuscate) {
+        return Object.assign({}, newOptions, options.obfuscate, {
+            formattingType: 'obfuscation'
+        });
+    }
+    return newOptions as unknown as vscode.FormattingOptions;
+};
+const getRangeForDocument = (document: vscode.TextDocument) => {
+    const lastLine = document.lineCount - 1;
+    const lastLineObj = document.lineAt(lastLine);
+
+    const lastChar = lastLineObj.range.end.character;
+
+    const start = { character: 0, line: 0 };
+    const end = { character: lastChar, line: lastLine };
+
+    return new vscode.Range(start.line, start.character, end.line, end.character);
+};
+const formatDocument = (document: vscode.TextDocument) => {
+    try{
+        const lastLine = document.lineCount - 1;
+        const lastLineObj = document.lineAt(lastLine);
+        const lastChar = lastLineObj.range.end.character;
+        const range = new vscode.Range(0, 0, lastLine, lastChar);
+        console.log('formatDocument');
+        return [vscode.TextEdit.replace(range,formatSql(document.getText(), getFormatterOptions()))];
+    } catch (e){
+        throw new Error('Jotain meni pieleen');
+    }
+};
